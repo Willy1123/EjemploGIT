e230bb8a18c180451e910aa6c29f9a1dacbd663e
Zeus1123 <guillermoblascoperez@gmail.com>
2015-11-06 19:23:26 +0100

    Finalizada la pirámide invertida
    Falta darle la vuelta a la pirámide para ponerla en su posición correcta

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -8,17 +8,30 @@ import acm.program.GraphicsProgram;
  */
 
 public class Ejercicio1 extends GraphicsProgram{
-	
+
 	public void run() {
 		//cambio el tamaño de la ventana
 		setSize(800,600);
 		//llamo al método que dibuja la pirámide
 		pintaPiramide();
-		
+
 	}
-	
+
 	public void pintaPiramide() {
+		int numLadrillosBase = 14;
+		int anchoLadrillo = 30;
+		int altoLadrillo = 12;
 		
+		//querido yo del martes:
+		//te falta lo mas facil de todo
+		//girar la piramide y ponerla bien
+		for(int j=0; j < numLadrillosBase; j++) {
+			for(int i=0; i < numLadrillosBase-j; i++) {
+				GRect ladrillo = new GRect(anchoLadrillo, altoLadrillo);
+				//añade una fila de ladrillos
+				add(ladrillo,anchoLadrillo*j/2 + anchoLadrillo*i, j*altoLadrillo);
+			}
+		}
 	}
-	
+
 }
