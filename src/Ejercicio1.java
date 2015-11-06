399446a55ef07bd93e509d7f0ccf2511542d0c3b
Zeus1123 <guillermoblascoperez@gmail.com>
2015-11-06 17:40:54 +0100

    Inicio del método PintaPiramide
    Inicio del método para Pintar la pirámide

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -1,12 +1,24 @@
 import acm.graphics.*;
 import acm.program.GraphicsProgram;
 
+/*
+ * Autor: Guillermo Blasco
+ * 
+ * Ejemplo de uso de GitHub
+ */
+
 public class Ejercicio1 extends GraphicsProgram{
 	
 	public void run() {
-		GLabel etiqueta = new GLabel("HOLA GIT");
-		add(etiqueta);
-		etiqueta.setLocation(100, 100);
+		//cambio el tamaño de la ventana
+		setSize(800,600);
+		//llamo al método que dibuja la pirámide
+		pintaPiramide();
+		
+	}
+	
+	public void pintaPiramide() {
+		
 	}
 	
 }
