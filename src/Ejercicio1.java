03bb461bf3938e42ea4ee135cd08b5ea9491cee3
Zeus1123 <guillermoblascoperez@gmail.com>
2015-11-06 17:42:32 +0100

    Revert "Inicio del método PintaPiramide"
    This reverts commit 399446a55ef07bd93e509d7f0ccf2511542d0c3b.

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -1,24 +1,12 @@
 import acm.graphics.*;
 import acm.program.GraphicsProgram;
 
-/*
- * Autor: Guillermo Blasco
- * 
- * Ejemplo de uso de GitHub
- */
-
 public class Ejercicio1 extends GraphicsProgram{
 	
 	public void run() {
-		//cambio el tamaño de la ventana
-		setSize(800,600);
-		//llamo al método que dibuja la pirámide
-		pintaPiramide();
-		
-	}
-	
-	public void pintaPiramide() {
-		
+		GLabel etiqueta = new GLabel("HOLA GIT");
+		add(etiqueta);
+		etiqueta.setLocation(100, 100);
 	}
 	
 }
